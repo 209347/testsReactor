947a1174db353729c383621205d1648c78fdfa69
Kalinowski.Mikołaj <209347@edu.p.lodz.pl>
2019-05-16 17:14:47 +0200

    add ifAuthorizationFailsWithdrawThrowsAtmException

--- testsReactor/src/test/java/edu/iis/mto/testreactor/exc3/AtmMachineTest.java
+++ testsReactor/src/test/java/edu/iis/mto/testreactor/exc3/AtmMachineTest.java
@@ -1,15 +1,64 @@
 package edu.iis.mto.testreactor.exc3;
 
 import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.is;
 import static org.junit.Assert.assertThat;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
+import org.junit.Before;
 import org.junit.Test;
+import org.mockito.Mockito;
+
+import javax.naming.AuthenticationException;
+import java.util.ArrayList;
+import java.util.List;
 
 public class AtmMachineTest {
+
+    private AtmMachine atmMachine;
+
+    private CardProviderService cardProviderService;
+
+    private AuthenticationToken authenticationToken;
+
+    private BankService bankService;
+
+    private MoneyDepot moneyDepot;
+
+    @Before
+    public void setup() {
+        cardProviderService = mock(CardProviderService.class);
+        bankService = mock(BankService.class);
+        moneyDepot = mock(MoneyDepot.class);
+        atmMachine = new AtmMachine(cardProviderService, bankService, moneyDepot);
+    }
 
     @Test
     public void itCompiles() {
         assertThat(true, equalTo(true));
+    }
+
+    @Test
+    (expected = AtmException.class)
+    public void ifAuthorizationFailsWithdrawThrowsAtmException() {
+        Money money = Money.builder()
+                           .withAmount(100)
+                           .withCurrency(Currency.PL)
+                           .build();
+
+        Card card = Card.builder()
+                        .withCardNumber("123")
+                        .withPinNumber(123)
+                        .build();
+
+        try {
+            Mockito.when(cardProviderService.authorize(card)).thenThrow(CardAuthorizationException.class);
+        } catch (CardAuthorizationException e) {
+            e.printStackTrace();
+        }
+
+        atmMachine.withdraw(money, card);
     }
 
 }
